b22f9146fa2f7b9fb6e26a5aba81b7dd0aa0ca77
Dong Soo Michael Seo <ds2242@cornell.edu>
2019-12-29 04:24:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var emptyArray = [];
+  var i;
+  for (i=0; i< 4; i++){
+    emptyArray.push(musicians[i]+" plays "+instruments[i]);
+  }
+  return emptyArray;
+}
+
